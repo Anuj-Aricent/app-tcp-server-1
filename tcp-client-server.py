d5336d997ba14b8e4c85dc5015c9ff1f4b0156d6
Anuj-Aricent <34433129+Anuj-Aricent@users.noreply.github.com>
2018-01-05 12:17:02 +0530

    Update tcp-client-server.py

--- tcp-client-server.py
+++ tcp-client-server.py
@@ -10,7 +10,7 @@ host_server = "0.0.0.0"
 port_server = 12341
 server.bind((host_server, port_server))
 url_addrs_json = eval(redis_db.get('TCP-SERVER-MS'))
-url_addrs = str(url_addrs_json["12345"])
+url_addrs = str(url_addrs_json["12346"])
 client = socket.socket()
 addr = urlparse(url_addrs)
 host_client = addr.hostname # Get local machine name
